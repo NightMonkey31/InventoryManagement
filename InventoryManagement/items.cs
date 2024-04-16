e69e7ce6cfdb280f0bdb5f040391d400c80b2205
MITHUN A <MITHUN A@DESKTOP-6SK4EHT>
2024-04-16 15:43:45 +0530

    Add project files.

---
+++ InventoryManagement/items.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagement
+{
+    internal class items : users
+    {
+
+        users users = new users();
+
+        public void NEWUSER()
+        {
+            users.newUser();
+        }
+
+        public void EXISTINGUSER()
+        {
+            users.existingUser();
+        }
+        
+        public List<string> item = new List<string>();
+        public void Additems()
+        {
+            Console.WriteLine("how many assets you want to add: ");
+            int x = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < x; i++)
+            {
+                Console.WriteLine($"enter asset {i + 1}");
+                string asset = Console.ReadLine();
+                item.Add(asset);
+
+            }
+            Console.WriteLine("Assets added successfully");
+        }
+
+        public void DisplayItems()
+        {
+            if (item.Count > 0)
+            {
+                foreach (var i in item)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Currently there are no assests in your inventory! :(");
+            }
+        }
+
+        public void DeleteItems()
+        {
+            Console.WriteLine("Enter the index of the asset to be deleted: ");
+            int x = int.Parse(Console.ReadLine());
+
+            if (x >= 0 && x <= item.Count)
+            {
+                item.RemoveAt(x);
+                Console.WriteLine("Asset has been removed successfully!!");
+            }
+            else
+            {
+                Console.WriteLine("Asset does not exist at the current index");
+            }
+
+        }
+        public void UpdateItems()
+        {
+            Console.WriteLine("Enter the index of the asset to be updated: ");
+            int x = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the name of the updated asset: ");
+            string asset = Console.ReadLine();
+
+            if (x >= 0 && x <= item.Count)
+            {
+                item[x] = asset;
+                Console.WriteLine("Asset has been updated successfully!!");
+            }
+            else
+            {
+                Console.WriteLine("Asset does not exist at the current index");
+            }
+
+        }
+
+    }
+}
---
+++ InventoryManagement/users.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace InventoryManagement
+{
+    internal class users
+    {
+
+        public void newUser()
+        {
+            int license_Key = 0000;
+
+            while (true)
+            {
+                Console.WriteLine("Enter UserName: ");
+                string user = Console.ReadLine();
+                Console.WriteLine("Enter Pin: ");
+                var pin = Console.ReadLine();
+
+
+                Console.WriteLine("Enter PassKey:");
+                int Key = int.Parse(Console.ReadLine());
+
+                if (license_Key == Key)
+                {
+                    Console.WriteLine($"Weclome to our Inventory Management {user}");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("The PassKey does not match!! Try Again!!!//n");
+                }
+            }
+
+        }
+
+        public void existingUser()
+        {
+
+            int key = 1111;
+
+            while (true)
+            {
+                Console.WriteLine("Enter username: ");
+                string user = Console.ReadLine();
+                Console.WriteLine("Enter Pin: ");
+                int passkey = int.Parse(Console.ReadLine());
+
+                if (passkey == key)
+                {
+                    Console.WriteLine($"Wecome back {user}");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Icorrect username / password. Please Try again");
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
