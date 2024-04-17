532e55a4566227c210061c6496584f536f7025c4
NightMonkey31 <anandmithun33@gmail.com>
2024-04-17 10:26:43 +0530

    updated the exit option in the front menu.

--- InventoryManagement/Program.cs
+++ InventoryManagement/Program.cs
@@ -10,71 +10,79 @@ namespace InventoryManagement
             Console.WriteLine("!!!!~~~~WELCOME TO ASSET MANAGEMENT~~~~!!!!");
             items additems = new items();
 
-            Console.WriteLine("*--------------------*");
-            Console.WriteLine("| 1. New User        |");
-            Console.WriteLine("| 2. Existing User   |");
-            Console.WriteLine("| 3. Exit            |");
-            Console.WriteLine("*--------------------*");
-            
-            Console.WriteLine("Select Option!");
-            string user = Console.ReadLine();
-
-            switch (user)
-            {
-                case "1":
-                    additems.NEWUSER();
-                    break;
-                case "2":
-                    additems.EXISTINGUSER();
-                    break;
-                default:
-                    Console.WriteLine("Choose from the 3 options");
-                    break;
-
-
-
-            }
-
             while (true)
             {
                 Console.WriteLine("*--------------------*");
-                Console.WriteLine("| 1. Add Assets      |");
-                Console.WriteLine("| 2. Display Assets  |");
-                Console.WriteLine("| 3. Delete Assets   |");
-                Console.WriteLine("| 4. Update Assets   |");
-                Console.WriteLine("| 5. Exit            |");
+                Console.WriteLine("| 1. New User        |");
+                Console.WriteLine("| 2. Existing User   |");
+                Console.WriteLine("| 3. Exit            |");
                 Console.WriteLine("*--------------------*");
-                Console.WriteLine("Enter your option:");
 
-                string option = Console.ReadLine();
+                Console.WriteLine("Select Option!");
+                string user = Console.ReadLine();
 
-                if (option == "1")
+                switch (user)
                 {
-                    additems.Additems();
+                    case "1":
+                        additems.NEWUSER();
+                        break;
+                    case "2":
+                        additems.EXISTINGUSER();
+                        break;
+
+                    case "3":
+                        Console.WriteLine("**~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~**");
+                        Console.WriteLine("||    !Thank you for using this!    ||");
+                        Console.WriteLine("**~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~**");
+                        Console.ReadLine();
+                        return;
+                    default:
+                        Console.WriteLine("Choose from the 3 options");
+                        break;
                 }
-                else if (option == "2")
+
+
+                while (true)
                 {
-                    additems.DisplayItems();
-                }
-                else if (option == "3")
-                {
-                    additems.DeleteItems();
-                }
-                else if (option == "4")
-                {
-                    additems.UpdateItems();
-                }
-                else if (option == "5")
-                {
-                    Console.WriteLine("**~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~**");
-                    Console.WriteLine("||    !Thank you for using this!    ||");
-                    Console.WriteLine("**~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~**");
-                    Console.ReadLine();
-                    break;
-                }
-                else
-                {
-                    Console.WriteLine("Choose from the above options");
+                    Console.WriteLine("*--------------------*");
+                    Console.WriteLine("| 1. Add Assets      |");
+                    Console.WriteLine("| 2. Display Assets  |");
+                    Console.WriteLine("| 3. Delete Assets   |");
+                    Console.WriteLine("| 4. Update Assets   |");
+                    Console.WriteLine("| 5. Exit            |");
+                    Console.WriteLine("*--------------------*");
+                    Console.WriteLine("Enter your option:");
+
+                    string option = Console.ReadLine();
+
+                    if (option == "1")
+                    {
+                        additems.Additems();
+                    }
+                    else if (option == "2")
+                    {
+                        additems.DisplayItems();
+                    }
+                    else if (option == "3")
+                    {
+                        additems.DeleteItems();
+                    }
+                    else if (option == "4")
+                    {
+                        additems.UpdateItems();
+                    }
+                    else if (option == "5")
+                    {
+                        Console.WriteLine("**~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~**");
+                        Console.WriteLine("||    !Thank you for using this!    ||");
+                        Console.WriteLine("**~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~**");
+                        Console.ReadLine();
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Choose from the above options");
+                    }
                 }
             }
         }
